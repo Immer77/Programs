03599c3b54fbf41fc2f7ae82cf958ec8df4fdc9c
Immer77 <71756091+Immer77@users.noreply.github.com>
2021-11-03 10:35:28 +0100

    Add files via upload

--- PWScript/Filhandtering.ps1
+++ PWScript/Filhandtering.ps1
@@ -22,3 +22,22 @@ Rename-Item -Path C:\School\Opgave1\fil.txt -NewName "fil2.txt"
 
 #Omdøbning af extensions, her ændres den til en .bak fil eller en .old
 Get-ChildItem C:\School\Opgave1\fil.txt | Rename-Item -NewName {$_.Name -replace '\.txt$','.bak'}
+
+#Kopier en fil eller mappe
+Copy-Item -Path C:\School\Opgave1\fil.txt -Destination C:\School\Opgave1\fil2.txt
+
+#Kopierer alle filer til en bestemt lokation
+Copy-Item -Filter *.txt -Path C:\School\Opgave1
+
+#Flyt en fil
+Move-Item -Path C:\School\Opgave1\fil.txt -Destination C:\School\fil.txt
+
+#Flyt en mappe
+Move-Item -Path C:\School\Opgave1 -Destination C:\School\
+
+#Skriv data ind i en fil
+$data = "Dette er min data"
+Add-Content C:\School\Opgave1\fil.txt $data
+
+#Zip en mappe
+Compress-Archive -Path C:\School\*- -DestinationPath C:\School\school.Zip
