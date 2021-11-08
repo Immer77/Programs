f4e6d69f16f2c901a60b5e0b4864b377e5379eb3
Immer77 <71756091+Immer77@users.noreply.github.com>
2021-11-08 11:23:40 +0100

    Add files via upload

--- PWScript/PSSesh-Remote.ps1
+++ PWScript/PSSesh-Remote.ps1
@@ -2,8 +2,11 @@
 $cred = Get-Credential
 
 #Assign Computer variable med computernavn
-$computer = "PBI-SDC01"
+$computer = "PBI-SDC01", "PBI-FIL01"
 
 #Her connecter vi til sessionen.
 $sesh = New-PSSession -ComputerName $computer
 Enter-PSSession $sesh -Credential $cred
+
+#Invoke-Command kør script på remote PC
+Invoke-Command -ComputerName | ForEach-Object $computer -ScriptBlock $Scriptpath
