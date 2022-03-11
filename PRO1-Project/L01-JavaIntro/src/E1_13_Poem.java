151a88963a0c3f1c99eb70422c6ac953dcdecb75
Peter Immersen <71756091+Immer77@users.noreply.github.com>
2022-03-11 10:06:27 +0100

    Delete PRO1-Project directory

--- PRO1-Project/L01-JavaIntro/src/E1_13_Poem.java
+++
@@ -1,5 +0,0 @@
-public class E1_13_Poem {
-    public static void main(String[] args) {
-        System.out.println("Roser er røde \nVioler er blå\nJeg har en storetå");
-    }
-}
--- PRO1-Project/L01-JavaIntro/src/E1_8_robot.java
+++
@@ -1,5 +0,0 @@
-public class E1_8_robot {
-    public static void main(String[] args) {
-        System.out.println("  /////\n +\"\"\"\"\"+\n(| o o |)\n |  ^  |\n | '-' |\n +-----+");
-    }
-}
